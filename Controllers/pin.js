b455416248f08c6c442d4ba4c20962c9b88d8b68
Aderinola Taiwo <99713425+Yungkingdre@users.noreply.github.com>
2025-05-21 10:37:38 +0100

    updatepin

--- Controllers/pin.js
+++ Controllers/pin.js
@@ -1,4 +1,4 @@
-const regUser = require('../Models/registeration');
+const {regUser} = require('../Models/registeration');
 const bcrypt = require('bcryptjs');
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
