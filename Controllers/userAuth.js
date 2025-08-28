238a40ca3ed7c72269e4ab062f8c9335231bda41
Aderinola Taiwo <99713425+Yungkingdre@users.noreply.github.com>
2025-08-28 14:39:56 +0100

    update remove beneficiarey

--- Controllers/userAuth.js
+++ Controllers/userAuth.js
@@ -101,15 +101,15 @@ exports.removeBeneficiary = async (req, res) => {
         return res.status(403).json({ message: "Forbidden: You are not authorized to remove beneficiary" });
     }
     const beneficiaryId = req.params.id;
-    const beneficiary = await Beneficiary.findById(beneficiaryId);
-    if (!beneficiary) {
 
+    const beneficiary = await regUser.findById(beneficiaryId);
+    if (!beneficiary) {
         return res.status(404).json({ message: "Beneficiary not found" });
     }
-    if (beneficiary.userId.toString() !== currentUser._id.toString()) {
-        return res.status(403).json({ message: "Forbidden: You can only remove your own beneficiaries" });
-    }
-    await Beneficiary.findByIdAndDelete(beneficiaryId);
+    // if (benefactor.userId.toString() !== currentUser._id.toString()) {
+    //     return res.status(403).json({ message: "Forbidden: You can only remove your own beneficiaries" });
+    // }
+    await Beneficiary.findOneAndDelete({email: beneficiary.email});
     res.status(200).json({ message: "Beneficiary removed successfully" });
   } catch (error) {
     res.status(500).json({ message: error.message });
